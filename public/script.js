9fed654dc193f126cb1c12719a6f09cc340c513a
NetmareX <andrewjkorp@gmail.com>
2025-09-29 18:51:28 -0500

    Revert "Update script.js"
    This reverts commit f7defb740d7241278a5e618a3c231ef5b3e54f6d.

--- public/script.js
+++ public/script.js
@@ -433,7 +433,7 @@ function clearUrl(e) {
       window.location.pathname + window.location.search,
     );
 
-    // Yes this kinda just assumes it's got one but eh, should never be falsely fired and even if it does nothing bad should really happen
+    // Yes this kinda just assumes it's got one but eh, should never be falsely fired and even if it does nothing bad should really happe
     ElStore.get(EL_ID_LIST.clrCont).classList.remove(
       ElAttributes.activeClassName,
     );
